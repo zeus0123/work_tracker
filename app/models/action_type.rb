728afbc6185470158fc7ea554e2089ddd227268b
Trishan Ghosh <ghoshtrishan73@gmail.com>
2021-06-05 20:00:43 +0530

    added user type and action type model

---
+++ app/models/action_type.rb
@@ -0,0 +1,11 @@
+# == Schema Information
+#
+# Table name: action_types
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class ActionType < ApplicationRecord
+end
---
+++ test/models/action_type_test.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: action_types
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require "test_helper"
+
+class ActionTypeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
